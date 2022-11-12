3da92b77e6bb017897267884b7d19afc43395985
Krygore <webdev.krygore@gmail.com>
2022-11-12 11:43:39 -0800

    completed module project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,88 @@
 import React from 'react'
+import TodoList from "./TodoList"
+import Form from "./Form"
 
-export default class App extends React.Component {
+  class App extends React.Component {
+
+    constructor() {
+      super();
+      this.state = {
+        todos: [
+          {
+            task: 'Organize Garage',
+            id: 12345,
+            completed: false
+          },
+          {
+            task: 'Bake Cookies',
+            id: 32412,
+            completed: false
+          },
+          {
+            task: 'Study BloomTech',
+            id: 12435,
+            completed: false
+          },
+          {
+            task: 'Feed the fish',
+            id: 43256,
+            completed: false
+          }
+        ]
+      }
+    }
+
+    clearTodos = e => {
+      e.preventDefault();
+      this.setState({
+        ...this.state, 
+        todos: this.state.todos.filter(todo => {
+          return (todo.completed === false);
+        })
+      })
+    }
+
+    
+
+    addTodos = task => {
+
+      const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    }
+
+      this.setState({
+        ...this.state,
+        todos: [...this.state.todos, newTodo]
+      });
+    }
+
+    handleToggle = (clickedId) => {
+      this.setState({
+        ...this.state,
+        todos: this.todos.map(todo => {
+          if(todo.id === clickedId) {
+            return{
+              ...todo,
+              completed: !todo.completed
+          }
+          } else return todo;
+        })
+      })
+    }
+
   render() {
-    return (
-      <div>
-        Todo App
+    const { todos } = this.state;
+       return (
+      <div className="App">
+        <h1>TODOS</h1>
+          <TodoList  todos={todos}/>
+          <Form addTodos={this.addTodos}/>
+          <button onClick={this.clearTodos}>Clear</button>
       </div>
     )
   }
 }
+
+export default App;
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Todo from "./Todo";
 
-export default class TodoList extends React.Component {
+class TodoList extends React.Component {
   render() {
     return (
-      <div>
-        TodoList
-      </div>
+      <ul>
+        {
+        this.props.todos.map(todo => {
+          return (<Todo key= {todo.id} handleToggle={this.props.handleToggle} todo={this.props.todo} />)
+        })
+      }
+    </ul>
     )
   }
 }
+
+export default TodoList
